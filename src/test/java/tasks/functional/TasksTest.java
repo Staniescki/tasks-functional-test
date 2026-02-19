5112bf788e5d57e47bccc64d938e44309dc0aa15
Diego Staniescki <diego.stk@live.com>
2026-02-18 22:41:39 -0300

    Paralelismo em docker

--- src/test/java/tasks/functional/TasksTest.java
+++ src/test/java/tasks/functional/TasksTest.java
@@ -1,22 +1,29 @@
 package tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public class TasksTest {
 	
 	public WebDriver setup() {
-		WebDriverManager.chromedriver().setup();
-	    
-	    return new ChromeDriver();
+		
+	    try {
+	    	ChromeOptions options = new ChromeOptions();
+	    	return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 	
 	
@@ -27,7 +34,8 @@ public class TasksTest {
 	    
 	    try {
 	    	
-	    	driver.get("http://localhost:8001/tasks");
+	    	driver.get("http://172.26.144.1:8001/tasks");
+	    	
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			// clicar em add todo
@@ -62,7 +70,8 @@ public class TasksTest {
 		
 		try {
 			
-			driver.get("http://localhost:8001/tasks");
+			driver.get("http://172.26.144.1:8001/tasks");
+			
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			// clicar em add todo
@@ -83,6 +92,7 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
+	
 	@Test
 	public void naoDeveSalvarTarefaSemData() {
 		
@@ -90,7 +100,8 @@ public class TasksTest {
 		
 		try {
 			
-			driver.get("http://localhost:8001/tasks");
+			driver.get("http://172.26.144.1:8001/tasks");
+			
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			// clicar em add todo
@@ -113,11 +124,6 @@ public class TasksTest {
 	}
 	
 	
-	
-	
-	
-	
-	
 	@Test
 	public void naoDeveSalvarTarefaComDataPassada() {
 		
@@ -125,7 +131,8 @@ public class TasksTest {
 		
 		try {
 			
-			driver.get("http://localhost:8001/tasks");
+			driver.get("http://172.26.144.1:8001/tasks");
+			
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			// clicar em add todo
